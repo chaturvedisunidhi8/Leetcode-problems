faa973e996803ff2e3ffed8ae0a20adad06a08af
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-03-30 11:20:43 +0530

    Fix class declaration syntax in ROTATE ARRAY.cpp

--- ROTATE ARRAY.cpp
+++ ROTATE ARRAY.cpp
@@ -1,6 +1,6 @@
 //Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 
-class Solution {
+class Solution { 
 public:
     void rotate(vector<int>& nums, int k) {
         k%= nums.size();
