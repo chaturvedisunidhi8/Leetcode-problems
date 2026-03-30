4ea92b1ddcd86f3444973715dc116ce3ae76c6cf
Sunidhi Chaturvedi <chaturvedisunidhi8@gmail.com>
2026-03-30 11:20:58 +0530

    Update PRODUCT OF ARRAY EXCEPT SELF.cpp

--- PRODUCT OF ARRAY EXCEPT SELF.cpp
+++ PRODUCT OF ARRAY EXCEPT SELF.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n=nums.size();
-        vector<int> ans(n,1);
+        vector<int> ans(n,1); 
 
         for(int i=1;i<n;i++){
          ans[i]=ans[i-1]* nums[i-1];
